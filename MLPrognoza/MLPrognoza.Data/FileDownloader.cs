3482ece4769abf70c31670f3b105212fdf8f64ae
nlugic <nlugic@gmail.com>
2018-02-06 10:51:40 +0100

    Sistem cita podatke i smesta ih u niz radi dalje obrade, vidi handler pritiska na dugme get data, klasu WeatherModel, kao i staticke metode DownloadWeatherData, ConvertISHFile, GetWeatherModel/Record/StationData i ostale

--- MLPrognoza/MLPrognoza.Data/FileDownloader.cs
+++ MLPrognoza/MLPrognoza.Data/FileDownloader.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -12,98 +13,57 @@ namespace MLPrognoza.Data
     public class FileDownloader
     {
 
-        public static void DownloadWeatherData(string year, ProgressBar progress)
+        public static ICollection<string> DownloadWeatherData(int yearStart, int yearEnd, string station, ProgressBar progress)
         {
-            Directory.CreateDirectory(year);
-            //int max = 20;
-            string[] files = GetFileList(year);
-            progress.Minimum = 0;
-            progress.Maximum = files.Length;
-            progress.Step = 1;
-
-            for (int i = 0; i < files.Length; ++i)
-            {
-                Download(year, files[i]);
-                progress.PerformStep();
-                progress.Update();
-                //if (i == max)
-                //    break;
-                System.Threading.Thread.Sleep(2000);
-            }
-        }
-
-        public static string[] GetFileList(string year)
-        {
-            string[] downloadFiles;
-            StringBuilder result = new StringBuilder();
-            WebResponse response = null;
-            StreamReader reader = null;
+            ICollection<string> fileNames = new List<string>();
 
             try
             {
-                FtpWebRequest reqFTP;
-                reqFTP = (FtpWebRequest)WebRequest.Create(new Uri(@"ftp://ftp.ncdc.noaa.gov/pub/data/noaa/" + year + "/"));
-                reqFTP.Method = WebRequestMethods.Ftp.ListDirectory;
-                response = reqFTP.GetResponse();
-                reader = new StreamReader(response.GetResponseStream());
+                progress.Minimum = 0;
+                progress.Maximum = yearEnd - yearStart + 1;
+                progress.Step = 1;
 
-                string line = reader.ReadLine();
-                while (line != null)
+                for (int year = yearStart; year <= yearEnd; ++year)
                 {
-                    result.Append(line + "\n");
-                    line = reader.ReadLine();
-                }
-                
-                result.Remove(result.ToString().LastIndexOf('\n'), 1);
-                return result.ToString().Split('\n');
-            }
-            catch (Exception)
-            {
-                if (reader != null)
-                    reader.Close();
-                if (response != null)
+                    string fileName = year + "/" + station + "-" + year + ".gz";
+                    Directory.CreateDirectory(Path.GetDirectoryName(fileName));
+
+                    FtpWebRequest reqFileList = (FtpWebRequest)WebRequest.Create(
+                        new Uri(@"ftp://ftp.ncdc.noaa.gov/pub/data/noaa/" + fileName)
+                    );
+                    reqFileList.Method = WebRequestMethods.Ftp.DownloadFile;
+
+                    WebResponse response = reqFileList.GetResponse();
+                    Stream responseStream = response.GetResponseStream();
+                    FileStream downloadedFile = new FileStream(fileName, FileMode.Create);
+
+                    int length = 65536, bytesRead = 0;
+                    byte[] buf = new byte[length];
+                    while ((bytesRead = responseStream.Read(buf, 0, length)) > 0)
+                        downloadedFile.Write(buf, 0, bytesRead);
+
+                    downloadedFile.Close();
+
+                    string newFileName = fileName.Substring(0, fileName.Length - 3);
+                    fileNames.Add(newFileName);
+
+                    downloadedFile = new FileStream(fileName, FileMode.Open);
+
+                    FileStream decompressedFile = File.Create(newFileName);
+                    GZipStream decompression = new GZipStream(downloadedFile, CompressionMode.Decompress);
+                    decompression.CopyTo(decompressedFile);
+
+                    decompression.Close();
+                    decompressedFile.Close();
+                    downloadedFile.Close();
+                    responseStream.Close();
                     response.Close();
-                downloadFiles = null;
-                return downloadFiles;
-            }
-        }
 
-        private static void Download(string year, string file)
-        {
-            try
-            {
-                string uri = "ftp://ftp.ncdc.noaa.gov/pub/data/noaa/" + year + "/" + file;
-                Uri serverUri = new Uri(uri);
-                if (serverUri.Scheme != Uri.UriSchemeFtp)
-                    return;
-
-                FtpWebRequest reqFTP;
-                reqFTP = (FtpWebRequest)WebRequest.Create(new Uri("ftp://ftp.ncdc.noaa.gov/pub/data/noaa/" + year + "/" + file));
-                reqFTP.Method = WebRequestMethods.Ftp.DownloadFile;
-
-                FtpWebResponse response = (FtpWebResponse)reqFTP.GetResponse();
-                Stream responseStream = response.GetResponseStream();
-                FileStream writeStream = new FileStream(year + @"\" + file, FileMode.Create);
-
-                int Length = 2048;
-                byte[] buffer = new byte[Length];
-                int bytesRead = responseStream.Read(buffer, 0, Length);
-                while (bytesRead > 0)
-                {
-                    writeStream.Write(buffer, 0, bytesRead);
-                    bytesRead = responseStream.Read(buffer, 0, Length);
+                    progress.PerformStep();
                 }
-                writeStream.Close();
-                response.Close();
             }
-            catch (WebException wEx)
-            {
-                MessageBox.Show(wEx.Message, "Download Error");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Download Error");
-            }
+            catch (Exception) { }
+            return fileNames;
         }
 
     }
