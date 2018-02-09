ea83c9d187a585fd5b7acd81393d2b04f6106c15
nlugic <nlugic@gmail.com>
2018-02-09 23:28:27 +0100

    in

--- MLPrognoza/MLPrognoza.Data/Models/WeatherModel.cs
+++ MLPrognoza/MLPrognoza.Data/Models/WeatherModel.cs
@@ -9,13 +9,13 @@ namespace MLPrognoza.Data.Models
     public class WeatherModel
     {
 
-        public DateTime Time { get; set; } // vreme merenja, mozda suvisno
+        public DateTime Time { get; set; } // vreme merenja
         public int WindSpeed { get; set; } // brzina vetra u m/s
         public int Temperature { get; set; } // temperatura u C
-        public int DewPointTemperature { get; set; } // temperatura kondenzacije u C, mozda suvisno
+        public int DewPointTemperature { get; set; } // temperatura kondenzacije u C
         public float AtmosphericPressure { get; set; } // atmosferski pritisak
-        public float[] Precipitation { get; set; } // padavine u poslednjih sat/6/12/24 sata
-        public float SnowDepth { get; set; } // dubina snega, mozda suvisno
+        public float[] Precipitation { get; set; } // padavine u mm poslednjih 1/6/12/24 sata
+        public float SnowDepth { get; set; } // dubina snega u mm
 
         public WeatherModel(WeatherRecord record)
         {
